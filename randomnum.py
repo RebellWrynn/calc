726c2ef0d62bbe1e9649d4798206969f0db620e6
unknown <egzinivan@gmail.com>
2026-01-19 17:37:33 +0500

    2

--- randomnum.py
+++ randomnum.py
@@ -3,8 +3,18 @@ from time import time
 #def randomnum ():
 
 #return(a)
+def test1():
+    a = (time() / 129) - (time() // 129)
+    b = list(str(a))
+    c = b[-3:]
+    forint = map(str,c)
+    forint1 = ''.join(forint)
+    return forint1
 
-a = (time() / 129) - (time() // 129)
-b = list(str(a))
-c = b[-3:]
-print()
+def test2():
+    d = (time() / 129) - (time() // 129)
+    e = list(str(d))
+    f = e[-3:]
+    forint2 = map(str,f)
+    forint3 = ''.join(forint2)
+    return forint3
