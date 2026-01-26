0ab3b57a3a90b9d7b68a17c9f0cb6a3373c39fd3
unknown <egzinivan@gmail.com>
2026-01-26 19:37:00 +0500

    upgrade 2

--- randomnum.py
+++ randomnum.py
@@ -1,7 +1,8 @@
 from time import time
 
 def test():
-    a = (time() / 129) - (time() // 129)
+    current_time = time()
+    a = (current_time / 129) - (current_time // 129)
     b = list(str(a))
     c = b[-3:]
     forint = map(str,c)
